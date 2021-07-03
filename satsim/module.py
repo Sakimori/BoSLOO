41356f592206d3418abb3116fefd83bfaf2bad7f
Laika <Klingbeil@hyperobject.net>
2021-07-03 11:37:12 -0400

    prototype module

--- satsim/module.py
+++ satsim/module.py
@@ -3,7 +3,7 @@ class module:
 	id_c = 0
 	def __init__(self, name):
 		self.fields = {}
-		self.fields["name"] = "PRIMITIVE"
+		self.fields["name"] = name
 		self.id = module.id_c
 		module.id_c += 1
 
