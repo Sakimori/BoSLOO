7ef54e8de3766de848ad056dc161e2949caf8cc4
Sakimori <sakimorihalley@gmail.com>
2022-07-02 22:30:48 -0400

    boot-up screen active, state change on keypress

--- GroundControl/frontend/src/index.js
+++ GroundControl/frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Typing from 'react-typing-animation';
+import ClickableDiv from 'react-clickable-div';
+import KeyboardEventHandler from 'react-keyboard-event-handler'
 import './index.css';
 import logos from './BoSLOO logo.json';
 
@@ -124,34 +126,67 @@ class Terminal extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            lines: [
-                { text: 'BoSLOO v0.1', printed: false },
-                { text: 'Today is Saturday.', printed: false },
-                { text: logos.logo, printed: false}
-            ]
+            bodyObj: <SelfTest />,
+            init: false,
         }
     }
 
+    appendToBody(newContent) {
+        let bodyObj = JSON.parse(JSON.stringify(this.state.bodyObj)) + newContent;
+        this.setState({
+            bodyObj: bodyObj
+        });
+    }
+
+    replaceBody(newContent) {
+        this.setState({
+            bodyObj: newContent
+        });
+    }
+
+    handler() {
+        const init = this.state.init;
+        if (!init) { this.replaceBody('YIPPIE!'); this.setState({ init: true }); }
+    }
+
     render() {
-        var rawLines = [];
-        var typingLines = [];
-        for (let line of this.state.lines) {
-            line.printed ? rawLines = rawLines.concat([line.text]) : typingLines = typingLines.concat([line.text]);
-        }
-        const raw = rawLines.join('\n');
-        const type = typingLines.join('\n');
         return (
-            <div className='Terminal'>
-                {raw}
-                <Typing speed={2 }>
-                    {type}
-                </Typing>
+            <div className='terminal'>
+                {this.state.bodyObj}
+                <KeyboardEventHandler handleKeys={['all']}
+                    onKeyEvent={(key, e) => { if (!this.state.init) { this.replaceBody('YIPPIE!'); this.setState({ init: true }); } }} />
             </div>
         );
     }
 }
 
-// ========================================
+class SelfTest extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            logodisplay: false
+        }
+    }
+
+    render() {
+        const final = <span>{logos.logo} < br /><br /><br /><Typing speed={5}>Press any key to continue...<br/>> <span className='blink-text'>_</span></Typing></span>;
+        return (
+            <div className='power-on-self-test'>
+                <Typing speed={5} onFinishedTyping={() => this.setState({logodisplay: true})}>
+                BoSLOO ACPI BIOS v0.1<br />
+                Sakimori Ind. 2022<br />
+                Initializing cache.................................<Typing.Speed ms={200} />...<Typing.Speed ms={5} /> OK!<br />
+                Initializing network.........<Typing.Speed ms={500} />....<Typing.Speed ms={5} />.................<Typing.Speed ms={300} />....<Typing.Speed ms={5} /> OK!<br />
+                Initializing GPU...................................<Typing.Speed ms={1000} />...<Typing.Speed ms={5} /> <span className='fail-text'>FAIL!</span><br />
+                <br />             
+                </Typing>
+                {this.state.logodisplay ? final : null}
+            </div>
+        )
+    }
+}
+
+// =========================================
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Terminal />);
